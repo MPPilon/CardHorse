94ebbf9d59b80e5c31d0deb66776d61a8c32627f
MPPilon <mppilon@gmail.com>
2018-04-06 16:23:05 -0600

    Initial setup of API index page

--- CardHorse/HorseAPI/views.py
+++ CardHorse/HorseAPI/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def api_index(request):
+    ###
+    # This is the API Index page. It will contain instructions on how to access and use the Card Horse API.
+    # This view does not do any processing, and should never do any processing, as it is public and anonymously-facing.
+    ###
+    return HttpResponse("Hello, world. This is the API Index.")
